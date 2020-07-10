a032f249f76a53a954ed11bb1802e391e65b8a96
Daniel <bugbuilds@gmail.com>
2020-07-10 01:43:03 -0400

    added paper environment

--- gym_stock_trading/__init__.py
+++ gym_stock_trading/__init__.py
@@ -9,3 +9,9 @@ register(
     id='AlpacaStockTrading-v0',
     entry_point='gym_stock_trading.envs.alpaca_stock_trading_env:AlpacaStockTradingEnv'
 )
+
+register(
+    id='PaperAlpacaStockTrading-v0',
+    entry_point='gym_stock_trading.envs.paper_alpaca_stock_trading_env:PaperAlpacaStockTradingEnv'
+)
+
